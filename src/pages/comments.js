5d7356420dee43ec8dcf9e8d66ba437f4cb75f74
NaheedRayan <naheed27ray@gmail.com>
2024-01-09 14:09:46 +0600

    added functionality to pass data between pages

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -1,9 +1,26 @@
+import { useLocation } from "react-router-dom"
 
 
 export default function Comments(){
+
+    const location = useLocation() ;
+
     return(
+        <>
         <h1>This is comments page</h1>
+        <h3>{location.state.data.uniqueId}</h3>
+        <h3>{location.state.data.name}</h3>
+        <p>Email: {location.state.data.email}</p>
+        <p>Department: {location.state.data.department}</p>
+        <p>Designation: {location.state.data.designation}</p>
+        {/* Add more fields as needed */}
+        <img src={location.state.data.imageUrl} alt={`${location.state.data.name}'s avatar`} />
+        <p>Reviews Count: {location.state.data.reviewsCnt}</p>
+        <p>Ratings: {location.state.data.ratings}</p>
+        {/* Add more user details */}
+        </>
     )
 
 
 }
+
--- src/pages/scoreboard.js
+++ src/pages/scoreboard.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { collection, onSnapshot, query } from 'firebase/firestore';
 import firestore from "../firebase"
+import {Link, useNavigate} from 'react-router-dom';
+
+
 
 
 export default function Scoreboard(){
@@ -28,32 +31,40 @@ export default function Scoreboard(){
 
     }, [])// Empty dependency array means this effect runs once on mount
 
+    const cardClicked = (event , data)=>{
+        console.log('Card is clicked and its unique id is '+ data.uniqueId);
+      
+        
+    }
 
+    const navigate = useNavigate();
  
     return(
         <>
-        <h1>
-            This is scoreboard page
-        </h1>
+        <h1>This is scoreboard page</h1>
         {/* <div>Hello world  Naheed{data.map((item, index) => (
           <li key={index}>{JSON.stringify(item)}</li>
         ))}
         </div>   */}
-
         <h2>User List</h2>
         {data.map((data) => (
-        <div key={data.id} className="user-card">
-          <h3>{data.name}</h3>
-          <p>Email: {data.email}</p>
-          <p>Department: {data.department}</p>
-          <p>Designation: {data.designation}</p>
-          {/* Add more fields as needed */}
-          <img src={data.imageUrl} alt={`${data.name}'s avatar`} />
-          <p>Reviews Count: {data.reviewsCnt}</p>
-          <p>Ratings: {data.ratings}</p>
-          {/* Add more user details */}
-        </div>
-      ))}
+
+                
+            <div key={data.uniqueId} className="user-card" onClick={() => {navigate('/comments', {replace:false , state:{data:data}})}}>
+              <h3>{data.uniqueId}</h3>
+              <h3>{data.name}</h3>
+              <p>Email: {data.email}</p>
+              <p>Department: {data.department}</p>
+              <p>Designation: {data.designation}</p>
+              {/* Add more fields as needed */}
+              <img src={data.imageUrl} alt={`${data.name}'s avatar`} />
+              <p>Reviews Count: {data.reviewsCnt}</p>
+              <p>Ratings: {data.ratings}</p>
+              {/* Add more user details */}
+            </div>
+        
+        ))
+        }
         </>
 
 
